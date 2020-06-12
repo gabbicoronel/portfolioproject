e74c4fe3a8b25bade506d0d531b9a536f09e816f
Gabby <gabbycoronel@gmail.com>
2020-06-12 00:06:44 -0700

    hoverimage &maplinks

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(function(){
+    $("#newsButton").click(function(){
+        $("#letterModal").modal("show");
+    });
+}); 
+
+$(function(){
+    $("#discountButton").click(function(){
+        $("#letterModal").modal("show");
+    });
+});
+
+
+
+
+
+
+$(document).ready(function(){
+    $("#imgSurf").hover(function(){
+      $('.hero-image').css(backgroundImage = url('images/img_9127.jpeg'));
+      });
+    });
+    
+    
